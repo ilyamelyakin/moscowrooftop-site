a99172ffda7ea4389000ff80d4e50f3f3e6d1b27
Ilya Melyakin <ilyamelyakin@users.noreply.github.com>
2026-07-05 17:23:09 +0300

    Run Worker before assets for redirects

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
+    const forwardedProto = request.headers.get("x-forwarded-proto");
+    const cfVisitor = request.headers.get("cf-visitor") || "";
+    const isHttp =
+      url.protocol === "http:" ||
+      forwardedProto === "http" ||
+      cfVisitor.includes('"scheme":"http"');
 
-    if (url.protocol === "http:" || url.hostname === "www.moscowrooftop.ru") {
+    if (isHttp || url.hostname === "www.moscowrooftop.ru") {
       url.protocol = "https:";
       url.hostname = "moscowrooftop.ru";
       return Response.redirect(url.toString(), 301);
     }
 
-    return env.ASSETS.fetch(request);
+    if (url.pathname === "/index.html") {
+      url.pathname = "/";
+      return Response.redirect(url.toString(), 301);
+    }
+
+    const assetUrl = new URL(request.url);
+    const lastSegment = assetUrl.pathname.split("/").pop();
+
+    if (assetUrl.pathname.endsWith("/")) {
+      assetUrl.pathname = `${assetUrl.pathname}index.html`;
+    } else if (lastSegment && !lastSegment.includes(".")) {
+      assetUrl.pathname = `${assetUrl.pathname}/index.html`;
+    }
+
+    return env.ASSETS.fetch(new Request(assetUrl.toString(), request));
   },
 };
